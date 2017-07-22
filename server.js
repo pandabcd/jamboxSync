90e706ab2fcf6f142194edb7365a74ab444af001
Subhodeep Maji <smsubhodeep16@gmail.com>
2017-07-23 00:31:44 +0530

    Loading html file using node js

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ var fs = require('fs') ;
 
 var server = http.createServer(function (req, res) {
   console.log('request was made: ' + req.url) ;
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
 
-    var myReadStream = fs.createReadStream(__dirname + "/" + 'readMe.txt', 'utf8') ;
+    var myReadStream = fs.createReadStream(__dirname + '/' + 'index.html', 'utf8') ;
     myReadStream.pipe(res) ;
 
-  res.end('Hello World\n');
+  // res.end('Hello World\n');
 });
 
 
-server.listen(1337, "127.0.0.1") ;
+server.listen(1338, "127.0.0.1") ;
 console.log('Server running at http://127.0.0.1:1337/');
 
 
