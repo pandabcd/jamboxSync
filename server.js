aee18cf413c97639601c6046c5464dde9f943995
Subhodeep Maji <smsubhodeep16@gmail.com>
2017-07-23 12:37:44 +0530

    opening page according to url

--- server.js
+++ server.js
@@ -5,16 +5,27 @@ var fs = require('fs') ;
 
 var server = http.createServer(function (req, res) {
   console.log('request was made: ' + req.url) ;
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  
 
+  if(req.url==='/home' || req.url==='/'){
+    console.log(req.url) ;
+    res.writeHead(200, {'Content-Type': 'text/html'});
     var myReadStream = fs.createReadStream(__dirname + '/' + 'index.html', 'utf8') ;
     myReadStream.pipe(res) ;
+    // res.end() ;
+  }
+
+  else{
+    res.end("hello world") ;
+  }
+
+    
 
   // res.end('Hello World\n');
 });
 
 
-server.listen(1338, "127.0.0.1") ;
+server.listen(1337, "127.0.0.1") ;
 console.log('Server running at http://127.0.0.1:1337/');
 
 
